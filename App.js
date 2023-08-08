288f774951e37338e28274c946054ba2d368c72f
John Volpitta <johnvolpitta@gmail.com>
2023-08-07 20:17:18 -0700

    starting awesome project to learn react native

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, FlatList, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { styles } from "./Styles/Styling";
+import { ColorBox } from "./Components/ColorBox";
 
 export default function App() {
+  const COLORS = [
+    { colorName: "Base03", hexCode: "#002b36" },
+    { colorName: "Base02", hexCode: "#073642" },
+    { colorName: "Base01", hexCode: "#586e75" },
+    { colorName: "Base00", hexCode: "#657b83" },
+    { colorName: "Base0", hexCode: "#839496" },
+    { colorName: "Base1", hexCode: "#93a1a1" },
+    { colorName: "Base2", hexCode: "#eee8d5" },
+    { colorName: "Base3", hexCode: "#fdf6e3" },
+    { colorName: "Yellow", hexCode: "#b58900" },
+    { colorName: "Orange", hexCode: "#cb4b16" },
+    { colorName: "Red", hexCode: "#dc322f" },
+    { colorName: "Magenta", hexCode: "#d33682" },
+    { colorName: "Violet", hexCode: "#6c71c4" },
+    { colorName: "Blue", hexCode: "#268bd2" },
+    { colorName: "Cyan", hexCode: "#2aa198" },
+    { colorName: "Green", hexCode: "#859900" },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView>
+        <FlatList
+          style={styles.container}
+          data={COLORS}
+          keyExtractor={(item) => item.hexCode}
+          renderItem={({ item }) => (
+            <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
+          )}
+          ListHeaderComponent={<Text style={styles.heading}>Solarize</Text>}
+        />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Styles/Styling.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  cyanBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#2aa198",
+  },
+  blueBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#268bd2",
+  },
+  magentaBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#d33682",
+  },
+  orangeBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#cb4b16",
+  },
+});
